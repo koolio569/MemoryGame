4e306851f10131a6f3da3f734d9c7f9bd9d1e3d0
koolio569 <jcampbell2801@gmail.com>
2018-05-28 14:28:15 +0100

    Created game.

---
+++ MemoryGame/CMemoryGame.vb
@@ -0,0 +1,128 @@
+﻿Public Class CMemoryGame
+
+    Private _difficulty As Integer
+
+    Private _possibleWords As List(Of String)
+
+    Private _grid As List(Of String)
+
+    Private _added As String
+
+    Private _removed As String
+
+    Sub New(ByVal difficulty As Integer, ByVal filePath As String)
+
+        _difficulty = difficulty
+
+        Dim wordlist_file As New System.IO.StreamReader(filePath, True)
+
+        While Not wordlist_file.EndOfStream
+
+            _possibleWords.Add(wordlist_file.ReadLine)
+
+        End While
+
+        wordlist_file.Close()
+
+        _possibleWords = _possibleWords.OrderBy(Function() Rnd()).ToList
+
+        For i = 0 To (_difficulty ^ 2) - 1
+
+            _grid.Add(_possibleWords(i))
+
+        Next
+
+    End Sub
+
+    Sub PlayGame()
+
+        Draw()
+
+        SwapWords()
+
+        Draw()
+
+        Console.WriteLine("What word was added?")
+        CheckAnswer(_added)
+
+        Console.WriteLine("What word was removed?")
+        CheckAnswer(_removed)
+
+        Console.WriteLine("winner!!!")
+        Console.ReadKey()
+
+    End Sub
+
+    Sub Draw()
+
+        Console.Clear()
+
+        For i = 1 To _grid.Count
+
+            If i Mod _difficulty = 0 Then
+
+                Console.WriteLine()
+
+                Console.Write(_grid(i - 1))
+
+            Else
+
+                Console.Write(_grid(i - 1))
+
+            End If
+
+        Next
+
+        Threading.Thread.Sleep(30000)
+
+    End Sub
+
+    Sub SwapWords()
+
+        Dim RandomInt As Integer
+
+        RandomInt = Math.Floor(Rnd() * (_grid.Count - 1))
+
+        _removed = _grid(RandomInt)
+
+        _grid.RemoveAt(RandomInt)
+
+        RandomInt = Math.Floor(Rnd() * (_possibleWords.Count - 1))
+
+        _added = _possibleWords(RandomInt)
+
+        _grid.Add(_possibleWords(RandomInt))
+
+    End Sub
+
+    Sub CheckAnswer(ByVal answer As String)
+
+        Dim input As String = ""
+
+        Do Until input = answer
+
+            Console.Clear()
+
+            Console.WriteLine("Input answer")
+
+            input = Console.ReadLine
+
+            If input = answer Then
+
+                Console.WriteLine("correct")
+
+                Console.ReadKey()
+
+            Else
+
+                Console.WriteLine("incorrect")
+
+                Console.ReadKey()
+
+            End If
+
+        Loop
+
+    End Sub
+
+End Class
--- MemoryGame/Program.vb
+++ MemoryGame/Program.vb
@@ -1,7 +1,32 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+
+    Sub Main()
+
+        Dim choice As String = ""
+
+        Console.WriteLine("1. easy")
+
+        Console.WriteLine("2. hard")
+
+        choice = Console.ReadLine
+
+        If choice = "1" Then
+
+            Dim currentGame As New CMemoryGame(3, "words.txt")
+
+            currentGame.PlayGame()
+
+        ElseIf choice = "2" Then
+
+            Dim currentGame As New CMemoryGame(4, "words.txt")
+
+            currentGame.PlayGame()
+
+        End If
+
+
     End Sub
+
 End Module
